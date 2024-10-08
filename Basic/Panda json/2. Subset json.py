bd54b8abe74aa14619464b9b0cc5e2afff234957
Data-not-love <truongdinhcuongd1.2022@gmail.com>
2024-10-08 12:18:37 +0700

    Filter row

--- Basic/Panda json/2. Subset json.py
+++ Basic/Panda json/2. Subset json.py
@@ -2,28 +2,38 @@ import pandas as pd
 import json
 
 data_json = pd.read_json("F:/3.5 Years/First Year/Python/Pandas/Basic/output.json")
-data_frame_data_json = data_json.to_dict()
-
+data_json_limited = data_json.head(5)
+data_frame_data_json = data_json_limited.to_dict()
 print_json = json.dumps(data_frame_data_json, indent = 4)
 print(print_json)
+print (data_json.columns)
 
-name = data_json["Name"]
-name_to_json = name.to_dict()
+
+
+name = data_json["Name"] # data frame with one attribute
+limited_info = name.head(6)
+name_to_json = limited_info.to_dict()
 print_name_in_json = json.dumps(name_to_json, indent = 4)
 print (print_name_in_json)
 
 
-print (data_json.columns)
 # Survived
 
 # ko the dung head vs tail cho json cause json is not a dataframe
-death_or_alive = data_json["Survived"]
-death_or_alive_to_json = death_or_alive.to_dict()
+death_or_alive = data_json["Survived"] # data frame with one attribute
+limited_info_for_survival = death_or_alive.tail(7)
+death_or_alive_to_json = limited_info_for_survival.to_dict()
 print_death_or_alive_in_json = json.dumps(death_or_alive_to_json, indent=4)
 print (print_death_or_alive_in_json)
 
-information = data_json[["Survived", "Name" ]]
+
+
+information = data_json[["Survived", "Name","Sex","Age"]] # data frame with 4 attribute
+limited_information = information.tail(3)
 # in json 1 barn ghi json rieeng biet
-information_to_json = information.to_dict(orient='records')
+information_to_json = limited_information.to_dict(orient='records')
 print_information_to_json = json.dumps(information_to_json, indent=4)
 print(print_information_to_json)
+
+print(type(information))
+
---
+++ Basic/Panda json/3. Filter Specific row.py
@@ -0,0 +1,52 @@
+import pandas as pd
+import json
+
+# LOAD JSON
+data_json = pd.read_json("F:/3.5 Years/First Year/Python/Pandas/Basic/output.json")
+
+
+limited_records_json = data_json.head(3)
+convert_json = limited_records_json.to_dict()
+print_in_json = json.dumps(convert_json, indent= 4 )
+print ( print_in_json)
+
+
+print (data_json.columns)
+
+
+information = data_json[(data_json["Sex"] == "male") &
+                        (data_json["Age"] >= str(53)) &
+                        (data_json["Pclass"] == 3) &
+                        (data_json["Fare"] > 20)]
+
+limited_information_json = information.head(5)
+info_with_condition = limited_information_json.to_dict(orient='records')
+print_information_in_json_4mat = json.dumps(info_with_condition, indent=5)
+print (print_information_in_json_4mat)
+
+print("Gioi han voi ham iloc")
+
+# chọn điều kiện lọc kết quả
+filter_conditions = data_json[(data_json["Embarked"] == "S") |
+                              (data_json["Sex"] == 'female') &
+                              (data_json["Age"] > str(30))]
+
+# chọn thuộc tính để hiển thị
+in4 = filter_conditions[["Name", "Embarked", "Sex", "Age"]]
+
+# giới hạn từ hàng 200 -> 205 rồi so sánh đk
+# có => in json
+# ko => []
+limited_in4 = in4.iloc[200:215]
+convert_2_json = limited_in4.to_dict(orient='records')
+print_in_json_4mat = json.dumps(convert_2_json, indent=5)
+print(print_in_json_4mat)
+
+
+filter_conditions_2 = data_json[(data_json["Sex"] == "female")]
+
+info_2 = filter_conditions_2[["Name", "Fare"]]
+limited_in4_2 = info_2.head(5)
+convert_json_2 = limited_in4_2.to_dict(orient='records')
+print_js = json.dumps(convert_json_2,indent=4)
+print(print_js)
