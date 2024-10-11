75b13daed0d594017b11c58a704ed42165f70df4
Data-not-love <truongdinhcuongd1.2022@gmail.com>
2024-10-11 14:04:18 +0700

    New line

--- Create Plot/Create Plot.py
+++ Create Plot/Create Plot.py
@@ -14,4 +14,6 @@ air_qlty.plot.scatter(x="station_london", y="station_paris", alpha=0.5)
 air_qlty.plot.box ()
 
 air_qlty.plot.area(figsize=(12, 4), subplots=True)
-plt.show()print ('noob at Panda')
+plt.show()
+print ('noob at Panda')
+print ('noob at Pandas')
