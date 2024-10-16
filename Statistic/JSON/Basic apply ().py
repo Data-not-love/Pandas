d00d83d2aa9948252088844408f6f037a70ac16a
Data-not-love <truongdinhcuongd1.2022@gmail.com>
2024-10-17 00:19:36 +0700

    Agg, groupby practice

--- Statistic/JSON/Basic apply ().py
+++ Statistic/JSON/Basic apply ().py
@@ -68,3 +68,79 @@ result = titanic[titanic["Sex"] == "female"].groupby("Pclass").agg(
     count_age_above_35 = pd.NamedAgg(column="Age", aggfunc="count")
 )
 print(result)
+
+
+#
+# mode != mean != count
+fare_above_40 = titanic[titanic["Fare"] > 40]
+result_40 = titanic.groupby("Embarked").agg(
+    mode_embarked=pd.NamedAgg(column="Sex", aggfunc=lambda x: x.mode()[0] if not x.mode().empty else None),  # Lấy giá trị mode đầu tiên (nếu có)
+    above_40 = pd.NamedAgg(column="Fare",aggfunc="count")
+)
+
+print(result_40)
+
+# Giá vé trung bình và trung vị của hành khách theo từng hạng ghế
+ticket = titanic.groupby("Pclass").agg(
+    mean = pd.NamedAgg(column="Fare",aggfunc="mean"),
+    median = pd.NamedAgg(column="Fare",aggfunc="median"))
+print (ticket)
+
+ticket_2 = titanic.groupby("Pclass")["Fare"].agg(["mean", "median"])
+print(ticket_2)
+# Tuổi trung bình và trung vị của các hành khách theo giới tính là bao nhiêu?
+Age = titanic.groupby("Sex").agg(
+    mean=pd.NamedAgg(column="Age", aggfunc="mean"),
+    median=pd.NamedAgg(column="Age", aggfunc="median")
+)
+print (Age)
+
+print ( titanic.groupby("Sex")["Age"].agg(["mean", "median"])
+)
+
+#Tuổi trung bình của những người sống sót là bao nhiêu?
+survivor = titanic[titanic["Survived"] == 1]
+result_survivor = titanic.groupby("Survived").agg(
+    mean=pd.NamedAgg(column="Age", aggfunc="mean"),
+)
+print (result_survivor)
+
+print(titanic[titanic["Survived"] == 1]["Age"].mean())
+#  Giá vé trung bình của những người sống sót là bao nhiêu?
+print(titanic[titanic["Survived"] == 1]["Fare"].mean())
+
+# Hạng ghế nào có tỷ lệ sống sót cao nhất?
+print (titanic.groupby("Pclass")["Survived"].mean())
+
+highest_survival_rate = titanic.groupby("Pclass").agg(
+    rate = pd.NamedAgg(column="Survived", aggfunc=lambda x:(x == 1).mean())
+)
+print(highest_survival_rate)
+
+# Có bao nhiêu hành khách không có thông tin về tuổi?
+NaN_age = titanic["Age"].isna().sum()
+print(NaN_age)
+
+# Có bao nhiêu hành khách không có thông tin về giá vé?
+NaN_fare = titanic["Fare"].isna().sum()
+print(NaN_fare)
+
+
+# Tỷ lệ phần trăm những người sống sót theo giới tính là bao nhiêu?
+survival_rate = titanic.groupby("Sex").agg(
+    rate = pd.NamedAgg(column="Survived",aggfunc=lambda x:(x==1).mean()*100)
+)
+print(survival_rate)
+print (titanic.groupby("Sex")["Survived"].mean()*100)
+
+
+# Tỉ lệ sống sót theo cả giới tính và hạng ghế (Pclass) là bao nhiêu?
+rate = titanic.groupby(["Sex","Pclass"]).agg(
+    rate = pd.NamedAgg(column="Survived",aggfunc=lambda x:(x==1).mean())
+)
+print(rate)
+
+print (titanic.groupby(["Sex", "Pclass"])["Survived"].mean()
+)
+
+# Trong mỗi hạng ghế, hành khách nam hay nữ có tỷ lệ sống sót cao hơn?
