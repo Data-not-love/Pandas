495fe66e49e094ec5ce642a7a1c1837409b92638
Data-not-love <truongdinhcuongd1.2022@gmail.com>
2024-10-17 10:23:32 +0700

    still not understand

--- Statistic/JSON/Practice 2.py
+++ Statistic/JSON/Practice 2.py
@@ -1,8 +1,14 @@
 import pandas as pd
 # Đối với mỗi nhóm Pclass và giới tính, sum tìm số lượng hành khách có giá vé > mean giá vé trung bình của nhóm đó.
+# tìm mean môi nhóm
 
 titanic = pd.read_json("F:/3.5 Years/First Year/Python/Pandas/Basic/titanic.json")
-titanic.groupby(["Pclass","Sex"]).agg(
+def function (dtframe):
+    mean_fare = dtframe["Fare"].mean()
+    return (dtframe["Fare"] > mean_fare).sum()
+    print(mean_fare)
 
-)
-print ( "Not done yet")
+
+print(titanic.groupby(["Pclass","Sex"]).apply(function,include_groups=False))
+
+print("Still don't understand")
