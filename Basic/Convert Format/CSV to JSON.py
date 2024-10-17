e39b30ea56af45d2ee074843407dd93fd5e9a357
Data-not-love <truongdinhcuongd1.2022@gmail.com>
2024-10-18 06:31:26 +0700

    one line

--- Basic/Convert Format/CSV to JSON.py
+++ Basic/Convert Format/CSV to JSON.py
@@ -26,4 +26,5 @@ reformat_output = reformat_path(output_json)
 to_json = csv_read.to_dict(orient='records')
 with open(reformat_output, 'w') as json_file:
     json.dump(to_json, json_file, indent=5)
+print("SUCCESSFULLY CONVERT")
 print("Upgrade a little bit!")
