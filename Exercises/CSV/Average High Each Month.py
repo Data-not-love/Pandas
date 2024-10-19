4116374ef192ba577d01b129e4aeb1cf2b445906
Data-not-love <truongdinhcuongd1.2022@gmail.com>
2024-10-19 17:32:20 +0700

    New Visualizations

---
+++ Exercises/CSV/Average High Each Month.py
@@ -0,0 +1,10 @@
+# mean high của Facebook các tháng trong năm 2010
+import pandas as pd
+import matplotlib.pyplot as plt
+from datetime import datetime
+
+
+dtFrame = pd.read_csv("F:/3.5 Years/First Year/Python/Pandas/Datasets/CSV/archive/Facebook.csv")
+print (len(dtFrame))
+
+plt.show()
---
+++ Exercises/CSV/High Price in July.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from datetime import datetime
+import matplotlib.pyplot as plt
+
+dtFrame = pd.read_csv("F:/3.5 Years/First Year/Python/Pandas/Datasets/CSV/archive/Netflix.csv")
+print (len(dtFrame))
+
+# convert datafarme qua datetime
+dtFrame["Date"]= pd.to_datetime(dtFrame["Date"])
+
+start_date = datetime(2004, 7,1)
+end_date = datetime(2004,7,31)
+
+range = (dtFrame["Date"] >= start_date) & (dtFrame["Date"] <= end_date)
+
+dfRange = dtFrame[range]
+
+dfRange.plot.scatter(x="Date",y="High")
+plt.xticks(rotation=75)
+plt.grid(True)
+plt.xlabel("Trading dates in July")
+plt.ylabel("High Price ($)")
+plt.show()
