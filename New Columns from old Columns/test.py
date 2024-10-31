50191b17b2ec39753f5cc4f22974c0eaf8fc797a
Data-not-love <truongdinhcuongd1.2022@gmail.com>
2024-10-31 08:40:05 +0700

    New line

--- New Columns from old Columns/test.py
+++ New Columns from old Columns/test.py
@@ -10,7 +10,7 @@ air_quality_renamed = air_quality.rename(
     }
 )
 print (air_quality_renamed.iloc[34:39])
-#NO2 concentration of the station in London in mg/m
+# NO2 concentration of the station in London in mg/m
 air_quality["paris_mg_per_cubic"] = air_quality["station_paris"] * 1.882
 print (air_quality[["datetime","paris_mg_per_cubic","station_paris"]].head(5))
 
@@ -22,4 +22,6 @@ print (air_quality[["datetime","station_paris","station_london","paris/london"]]
 # khi đã thêm cột vào data frame => thành thuộc tính của dataframe
 air_quality.plot.bar(x="datetime",y="paris/london")
 plt.xticks(np.arange(0, len(air_quality), step=30),rotation=75)
+plt.title("Paris / London Ratio")
+plt.grid(True, alpha = 0.3)
 plt.show()
