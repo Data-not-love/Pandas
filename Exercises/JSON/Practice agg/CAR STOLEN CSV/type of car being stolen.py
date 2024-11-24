276c7301220bacce7673bcee5ad2d8244b696056
Data-not-love <truongdinhcuongd1.2022@gmail.com>
2024-11-24 20:33:37 +0700

    One Line

--- Exercises/JSON/Practice agg/CAR STOLEN CSV/type of car being stolen.py
+++ Exercises/JSON/Practice agg/CAR STOLEN CSV/type of car being stolen.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
-stolen_vehicles_dtframe = pd.read_csv("F:/3.5 Years/First Year/Python/Pandas/Datasets/CSV/Motor+Vehicle+Thefts+CSV/stolen_vehicles.csv")
+stolen_vehicles_dtframe = pd.read_csv("F:/3.5 Years/First Year/Python/Pandas/Datasets/CSV/Motor Vehicle Thefts CSV/stolen_vehicles.csv")
 print(stolen_vehicles_dtframe.columns)
-details_dtframe = pd.read_csv("F:/3.5 Years/First Year/Python/Pandas/Datasets/CSV/Motor+Vehicle+Thefts+CSV/make_details.csv")
+details_dtframe = pd.read_csv("F:/3.5 Years/First Year/Python/Pandas/Datasets/CSV/Motor Vehicle Thefts CSV/make_details.csv")
 print(details_dtframe.columns)
-location_dtframe = pd.read_csv("F:/3.5 Years/First Year/Python/Pandas/Datasets/CSV/Motor+Vehicle+Thefts+CSV/locations.csv")
+location_dtframe = pd.read_csv("F:/3.5 Years/First Year/Python/Pandas/Datasets/CSV/Motor Vehicle Thefts CSV/locations.csv")
 print(location_dtframe.columns)
 merge_dtframe = pd.merge(stolen_vehicles_dtframe,details_dtframe,on="make_id",how="outer")
 
