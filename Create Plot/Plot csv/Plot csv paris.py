7ffce99588145c98c5044fa3b8dba918b51ecdce
Data-not-love <truongdinhcuongd1.2022@gmail.com>
2024-12-19 23:35:44 +0700

    Fixed

--- Create Plot/Plot csv/Plot csv paris.py
+++ Create Plot/Plot csv/Plot csv paris.py
@@ -4,4 +4,5 @@ import matplotlib.pyplot as plt
 air_qlty = pd.read_csv("F:/3.5 Years/First Year/Python/Pandas/Create Plot/air_quality.csv")
 
 air_qlty["station_london"].plot()
+plt.grid(True, alpha = 0.5)
 plt.show()
