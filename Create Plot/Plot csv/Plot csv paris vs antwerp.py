a93f66ba1bb99c3a528472183b693e2fcf2085f8
Data-not-love <truongdinhcuongd1.2022@gmail.com>
2024-12-19 23:37:45 +0700

    Fixed

--- Create Plot/Plot csv/Plot csv paris vs antwerp.py
+++ Create Plot/Plot csv/Plot csv paris vs antwerp.py
@@ -3,4 +3,5 @@ import pandas as pd
 
 air_qlty = pd.read_csv("F:/3.5 Years/First Year/Python/Pandas/Create Plot/air_quality.csv")
 air_qlty.plot.scatter(x="station_paris", y="station_antwerp", alpha =0.5)
+plt.grid(True, alpha = 0.5)
 plt.show()
