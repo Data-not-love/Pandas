b79184b57fde20084098d93dec55b84aefcc9bb8
Data-not-love <truongdinhcuongd1.2022@gmail.com>
2024-12-20 12:26:07 +0700

    Fixed

--- Create Plot/Plot json/Plot json.py
+++ Create Plot/Plot json/Plot json.py
@@ -1,10 +1,11 @@
 import pandas as pd
 from datetime import datetime
 import json
-# dùng datetime để xử lý các thuộc tính datetime
 
+# dùng datetime để xử lý các thuộc tính datetime
 analyzed_json = pd.read_json("F:/3.5 Years/First Year/Python/Pandas/Create Plot/air_quality.json") # dataframe
 data_frame_to_json = analyzed_json.to_dict(orient="records")  # chuyen ve dictionary
+# chuyển về string để datetime
 json_format = json.dumps(data_frame_to_json, default=str, indent=4)
 print (json_format)
 
